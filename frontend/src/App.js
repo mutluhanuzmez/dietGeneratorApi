63d57e65254e8e031406108a9b9a5ff11dd4296e
furkanuzmez <uzmezfurkan@gmail.com>
2021-04-12 15:50:43 +0200

    login register components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import PageHead from './LayoutCo/PageHead'
 import Entrance from './LayoutCo/Entrance'
 import LoginForm from './LoginCo/loginForm'
+import RegisterForm from './LoginCo/RegisterForm'
 import Jumbo from './LayoutCo/Jumbo'
 import Container from 'react-bootstrap/Container'
 import { BrowserRouter as Router ,Route } from "react-router-dom";
@@ -18,6 +19,7 @@ function App() {
        
        <Route path='/' component = { Entrance} exact  />
        <Route path='/login' component = { LoginForm }  />
+       <Route path='/register' component = { RegisterForm }  />
 
 
 
